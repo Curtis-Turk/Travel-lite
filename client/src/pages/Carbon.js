92ab106ce21671173a5c8199e9794c4bfa3a9485
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-09-28 22:12:07 +0100

    updated carbon page

--- client/src/pages/Carbon.js
+++ client/src/pages/Carbon.js
@@ -4,6 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import { Autocomplete, DirectionsRenderer } from "@react-google-maps/api";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import TrainIcon from "@mui/icons-material/Train";
 import PublicIcon from "@mui/icons-material/Public";
 import "../index.css";
@@ -19,7 +20,8 @@ function Carbon() {
   const [directionRes, setDirectionRes] = useState(null);
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
-  const [travel_stops, setTravel_stops] = useState("");
+  const [locationA, setLocationA] = useState("");
+  const [locationB, setLocationB] = useState("");
   const origin = useRef();
   const destination = useRef();
 
@@ -41,11 +43,13 @@ function Carbon() {
       },
     });
     console.log(results);
+    console.log(results.request.destination.query);
     const currentRoute = results.routes[0].legs[0];
     setDirectionRes(results);
     setDistance(currentRoute.distance.text);
     setDuration(currentRoute.duration.text);
-    setTravel_stops(currentRoute.steps[0].transit.arrival_stop.name);
+    setLocationA(results.request.origin.query);
+    setLocationB(results.request.destination.query); 
   };
 
   // ----- Check if API is loading ----- //
@@ -58,7 +62,7 @@ function Carbon() {
   return (
     <>
       <div className="">
-        <div className="flex justify-center">
+        <div className="flex justify-center ">
           <Autocomplete>
             <input
               type="text"
@@ -67,6 +71,7 @@ function Carbon() {
               variant="outlined"
               size="small"
               ref={origin}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </Autocomplete>
           <Autocomplete>
@@ -77,6 +82,7 @@ function Carbon() {
               variant="outlined"
               size="small"
               ref={destination}
+              className="p-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </Autocomplete>
 
@@ -89,10 +95,10 @@ function Carbon() {
               <tr>
                 <th className=""></th>
                 <th className="">
-                  <PublicIcon />
+                  <PublicIcon /> Co2
                 </th>
                 <th className="">
-                  <AccessTimeIcon />
+                  <AccessTimeIcon /> Time
                 </th>
               </tr>
             </thead>
@@ -101,27 +107,36 @@ function Carbon() {
                 <td className="w-20 text-center">
                   <FlightTakeoffIcon />{" "}
                 </td>
-                <td className="w-48 h-20 text-center">{distance} Co2</td>
-                <td className="w-48 h-20 text-center">{duration} hours</td>
+                <td className="w-48 h-20 text-center">{distance} </td>
+                <td className="w-48 h-20 text-center">{duration} </td>
               </tr>
 
               <td className="text-center">
                 <TrainIcon />
               </td>
-              <td className="w-48 h-20 text-center">{distance} miles</td>
-              <td className="w-48 h-20 text-center">{duration} hours</td>
+              <td className="w-48 h-20 text-center">{distance} </td>
+              <td className="w-48 h-20 text-center">{duration} </td>
             </tbody>
           </table>
         </div>
-
-        <GoogleMap
-          center={center}
-          zoom={12}
-          mapContainerClassName="w-full h-screen"
-        >
-          <Marker position={center} />
-          {directionRes && <DirectionsRenderer directions={directionRes} />}
-        </GoogleMap>
+        <div className="flex justify-center">
+          <GoogleMap
+            center={center}
+            zoom={12}
+            mapContainerClassName="w-9/12 h-96 rounded-lg"
+          >
+            <Marker position={center} />
+            {directionRes && <DirectionsRenderer directions={directionRes} />}
+          </GoogleMap>
+        </div>
+        <div className="pt-6 flex justify-center">
+          <h3 className="text-green-600 underline font-poppins ">
+            Your Trip Details
+          </h3>
+        </div>
+        <div className="flex justify-center">
+          {locationA} <span><ArrowRightAltIcon /></span> {locationB}
+        </div>
       </div>
     </>
   );
