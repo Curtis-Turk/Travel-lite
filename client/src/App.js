4539420e12d71aad8b0b8ad8205a98f85bbd2d90
Curtis Turk <curtis.c.turk@outlook.com>
2022-09-29 11:20:32 +0100

    Added the initial about us page and route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Carbon from "./pages/Carbon";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   let activeClassName = "nav-active";
@@ -14,29 +15,29 @@ function App() {
     <>
       <Router>
         <nav>
-          <ul>
-            <li>
-              {" "}
-              <NavLink
-                to="/"
-                className={({ isActive }) => isActive && activeClassName}
-              >
-                Home{" "}
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/carbon"
-                className={({ isActive }) => isActive && activeClassName}
-              >
-                Carbon{" "}
-              </NavLink>
-            </li>
-          </ul>
+          <NavLink
+            to="/"
+            className={({ isActive }) => isActive && activeClassName}
+          >
+            Home{" "}
+          </NavLink>
+          <NavLink
+            to="/carbon"
+            className={({ isActive }) => isActive && activeClassName}
+          >
+            Carbon{" "}
+          </NavLink>
+          <NavLink
+            to="/aboutus"
+            className={({ isActive }) => isActive && activeClassName}
+          >
+            About us{" "}
+          </NavLink>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/carbon" element={<Carbon />} />
+          <Route path="/aboutus" element={<AboutUs />} />
         </Routes>
       </Router>
     </>
