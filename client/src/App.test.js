bb8ce8a922e1d720b229e684b5b21fae53cdb5c6
Curtis Turk <curtis.c.turk@outlook.com>
2022-10-03 12:56:03 +0100

    Added some tests for app

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the Title", () => {
+test("About us appears in the nav bar", () => {
   render(<App />);
-  const linkElement = screen.getByText("Travel-lite");
-  expect(linkElement).toBeInTheDocument();
+  const NavLink = screen.getByText("About us");
+  expect(NavLink).toBeInTheDocument();
+});
+test("Home appears in the nav bar", () => {
+  render(<App />);
+  const NavLink = screen.getByText("Home");
+  expect(NavLink).toBeInTheDocument();
+});
+test("Carbon appears in the nav bar", () => {
+  render(<App />);
+  const NavLink = screen.getByText("Carbon");
+  expect(NavLink).toBeInTheDocument();
 });
