87d41e7fc801faf91973c2d1149aa4c1679a8761
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-10-03 20:22:41 +0100

    style changes

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function Navigation() {
-  
   return (
-<>
+    <>
       <footer class="top-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
         <span class="text-md text-gray-500 sm:text-center dark:text-gray-400">
-         
           <Link to="/" class=" hover:bg-gray-200">
             Travel-Lite
           </Link>
-          
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
@@ -37,7 +34,7 @@ function Navigation() {
         </ul>
       </footer>
     </>
-  )
+  );
 }
 
 export default Navigation;
